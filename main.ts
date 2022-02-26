a720eb2b5efef580c2bb591f048eea44b43e29e5
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2022-02-25 20:05:45 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,36 @@
-input.onLightConditionChanged(LightCondition.Dark, function () {
-	
-})
-function blast () {
-    for (let index = 0; index < 4; index++) {
-        keyboard.type("")
+function jibble () {
+    for (let index = 0; index < 10; index++) {
+        if (5 < randint(0, 9)) {
+            for (let index = 0; index < randint(2, 6); index++) {
+                goleft()
+            }
+        } else {
+            for (let index = 0; index < randint(2, 6); index++) {
+                goright()
+            }
+        }
+        blast()
     }
 }
+function blast () {
+    for (let index = 0; index < 4; index++) {
+        keyboard.key("  ", KeyboardKeyEvent.Down)
+        keyboard.key("  ", KeyboardKeyEvent.Up)
+        pause(100)
+    }
+}
+function goleft () {
+    keyboard.functionKey(KeyboardFunctionKey.LeftArrow, KeyboardKeyEvent.Down)
+    keyboard.functionKey(KeyboardFunctionKey.LeftArrow, KeyboardKeyEvent.Up)
+}
+function goright () {
+    keyboard.functionKey(KeyboardFunctionKey.RightArrow, KeyboardKeyEvent.Down)
+    keyboard.functionKey(KeyboardFunctionKey.RightArrow, KeyboardKeyEvent.Up)
+}
+let droid = 1
+pause(100)
 forever(function () {
-	
+    if (1 == droid) {
+        jibble()
+    }
 })
